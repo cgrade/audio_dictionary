35db10030bbbab73896e704990141aa7bb1aaaf6
cgrade <gradeisageek@gmail.com>
2023-09-04 03:42:23 +0100

    transcribed.py code refactore

--- transcribe.py
+++ transcribe.py
@@ -1,22 +1,47 @@
+#!/usr/bin/env python3
+""" A module that receives an audio file/stream 
+    and transcribe to text (speech to text) using Deepgram API
+"""
+
+
+# importing the required dependencies
 from deepgram import Deepgram
 import asyncio
 from record_audio import record_stream
+from key import API_KEY
 
-API_KEY = $API_KEY
+
+# initializing Api key and Mimetype
+API_KEY = API_KEY
 MIMETYPE = 'audio/wav'
 
 
-async def record_stream1(duration=5):
-    # Record audio for the specified duration
+async def record_stream1(duration: int=5) -> str:
+    """ An Async func that transcribe audio/speech to text
+        using deepgram api and returns string
+            Param:
+                duration: an int that represnts the timeframe
+                          for the stream
+    """
+
+
+    # taking the voice/speech from the user
     FILE = record_stream(duration)
 
+
+    # instantiating the Deepgram Class
     dg = Deepgram(API_KEY)
+
+    # getting the audio/speech into the audio variable
     audio = open(FILE, 'rb')
 
+    # configuring the source(the audio/speech data) attribute 
     source = {
         'buffer': audio,
         'mimetype': MIMETYPE
     }
+
+    # awaiting for the deepgram to transcribe the audio/speech stream
     response = await asyncio.create_task(
         dg.transcription.prerecorded(
             source,
@@ -27,6 +52,7 @@ async def record_stream1(duration=5):
         )
     )
 
+    # extracting the transcribed text from the response
     transcripts = response['results']['channels'][0]['alternatives'][0]['transcript']
     return transcripts
 
